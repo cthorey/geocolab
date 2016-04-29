6040bb5bba3d4e43bfa429149649ab56b1d68f2c
cthorey <clement.thorey@gmail.com>
2016-04-29 12:32:56 +0200

    ok

--- webapp/static/js/jquery-helper.js
+++ webapp/static/js/jquery-helper.js
@@ -38,14 +38,3 @@ function  ajaxCallNbAbstrats(nb,query)
     });     
 }
 
-
-function refreshTogleNbAbstract()
-// Helper to refresh the nb of abstract in the togle
-{
-    $("#dropdown-nb-abstracts li a").click(function(){
-        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-        var nb = $(this).parents('.dropdown').find('.btn').val();
-        ajaxCallNbAbstrats(nb);
-    });
-}
