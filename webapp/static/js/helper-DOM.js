eb5262bebf9face9c78e0c4184dda790641e713f
cthorey <clement.thorey@gmail.com>
2016-05-05 17:38:31 +0200

    Further work needed
    Former-commit-id: bdd1df9916deeed46df1ee0db8ba381574605458

--- webapp/static/js/helper-DOM.js
+++ webapp/static/js/helper-DOM.js
@@ -1,14 +1,3 @@
-function refreshTogleNbAbstract()
-// Helper to refresh the nb of abstract in the togle
-{
-    $("#dropdown-nb-abstracts li a").click(function(){
-        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-        var nb = $(this).parents('.dropdown').find('.btn').val();
-        ajaxCallNbAbstrats(nb);
-    });
-}
-
 function dropdownMenuNav()
 
 {
