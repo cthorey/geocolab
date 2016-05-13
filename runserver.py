3e10f1c9e938501115cda0d64afe983ac4c3a273
cthorey <clement.thorey@gmail.com>
2016-05-13 10:52:40 +0200

    ok
    Former-commit-id: 0e368d17cdc73df341e058ee28d1ec9b562bb023

--- runserver.py
+++ runserver.py
@@ -5,24 +5,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--prod', dest='prod',
                         action='store_true', help='run in prod?')
-    parser.add_argument('--port', dest='port', type=int,
-                        default=5000, help='port to serve on')
     args = parser.parse_args()
-    print args
-    if args.prod:
-        # run on Tornado instead, since running raw Flask in prod is not
-        # recommended
-        print 'starting tornado!'
-        from tornado.wsgi import WSGIContainer
-        from tornado.httpserver import HTTPServer
-        from tornado.ioloop import IOLoop
-        from tornado.log import enable_pretty_logging
-        enable_pretty_logging()
-        http_server = HTTPServer(WSGIContainer(app))
-        http_server.listen(args.port)
-        IOLoop.instance().start()
-        #app.run(host='0.0.0.0', threaded=True)
-    else:
-        print 'starting flask!'
-        app.run(port=args.port)
-        app.debug = True
+
+    print 'starting flask!'
+    app.run(port=args.port)
+    app.debug = True
